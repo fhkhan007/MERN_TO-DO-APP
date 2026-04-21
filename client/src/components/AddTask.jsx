707f12ee43b3d629700d92e2e756949535a4140d
MD: Fahad Hossain Khan <fahadhossainkhan@gmail.com>
2026-04-21 16:33:11 +0600

    modified list.js and addtask.jsx

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useState } from "react";
+import "../style/addtask.css";
+import { useNavigate } from "react-router-dom";
 
-const AddTask = () => {
+export default function AddTask() {
+  const [taskData, setTaskData] = useState({});
+  const navigate = useNavigate();
+
+  const handleAddTask = async () => {
+    if (!taskData?.title || !taskData?.description) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    let result = await fetch("http://localhost:3200/add-task", {
+      method: "POST",
+      body: JSON.stringify(taskData),
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    result = await result.json();
+
+    if (result.success) {
+      console.log("new task added");
+      navigate("/");
+    } else {
+      alert("try after sometime");
+    }
+  };
+
   return (
-    <div>AddTask</div>
-  )
-}
+    <div className="container">
+      <h1>Add New Task</h1>
 
-export default AddTask
+      <label>Title</label>
+      <input
+        onChange={(e) => setTaskData({ ...taskData, title: e.target.value })}
+        type="text"
+        placeholder="Enter task title"
+      />
+
+      <label>Description</label>
+      <textarea
+        onChange={(e) =>
+          setTaskData({ ...taskData, description: e.target.value })
+        }
+        rows={4}
+        placeholder="Enter task description"
+      />
+
+      <button onClick={handleAddTask} className="submit">
+        Add New Task
+      </button>
+    </div>
+  );
+}
--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+const [taskData, setTaskData] = useState([]);
+const [selectedTask, setSelectedTask] = useState([]);
 
-const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+const selectSingleItem = (id) => {
+  if (selectedTask.includes(id)) {
+    let items = selectedTask.filter((item) => item !== id);
+    setSelectedTask(items); // ✅ fixed
+  } else {
+    setSelectedTask([id, ...selectedTask]);
+  }
+};
 
-export default List
+const selectAll = (event) => {
+  if (event.target.checked) {
+    let items = taskData?.map((item) => item._id) || [];
+    setSelectedTask(items);
+  } else {
+    setSelectedTask([]);
+  }
+};
+
+const deleteMultiple = async () => {
+  let item = await fetch("http://localhost:3200/delete-multiple/", {
+    method: "DELETE",
+    credentials: "include",
+    body: JSON.stringify(selectedTask),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  item = await item.json();
+
+  if (item.success) {
+    getListData();
+    setSelectedTask([]); // ✅ reset selection
+  } else {
+    alert("Try after sometime");
+  }
+};
