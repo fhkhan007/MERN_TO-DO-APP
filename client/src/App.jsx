014a8a82a9c5bd9c3619f61e14330861e4a765f5
Md Fahad Hossain <fahadhossainkhan@gmail.com>
2026-04-27 11:41:27 +0600

    worked on client and server folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './style/app.css'
 import NavBar from './components/navbar'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import AddTask from './components/AddTask'
 import List from './components/List'
 import UpdateTask from './components/UpdateTask'
@@ -11,22 +11,21 @@ import Protected from './components/Protected'
 function App() {
   return (
     <>
-    {localStorage.getItem('login') && <NavBar />}
+      {/* Show NavBar only when user is logged in */}
+      {localStorage.getItem('login') && <NavBar />}
 
-    <Routes>
+      <Routes>
+        {/* Protected Routes */}
+        <Route element={<Protected />}>
+          <Route path="/" element={<List />} />
+          <Route path="/add" element={<AddTask />} />
+          <Route path="/update/:id" element={<UpdateTask />} />
+        </Route>
 
-      {/* Protected Routes */}
-      <Route element={<Protected />}>
-        <Route path='/' element={<List />} />
-        <Route path='/add' element={<AddTask />} />
-        <Route path='/update/:id' element={<UpdateTask />} />
-      </Route>
-
-      {/* Public Routes */}
-      <Route path='/signup' element={<SignUp />} />
-      <Route path='/login' element={<Login />} />
-
-    </Routes>
+        {/* Public Routes */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
